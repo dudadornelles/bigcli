0de17189a1a2a49a34d97538ff8965686cb3ef3d
Duda Dornelles <dudassdornelles@gmail.com>
2017-07-26 23:44:13 -0700

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -27,18 +27,13 @@ test_requirements = [
 
 setup(
     name='bigcli',
-    version='0.1.0',
-    description="A python framwork to write large CLIs using dependency injection, highly testable and reusable",
+    version='0.1.2',
+    description="A python framwork to write large CLIs using dependency injection",
     long_description=readme + '\n\n' + history,
     author="Duda Dornelles",
     author_email='duda@thoughtworks.com',
     url='https://github.com/dudadornelles/bigcli',
     packages=find_packages(include=['bigcli']),
-    entry_points={
-        'console_scripts': [
-            'bigcli=bigcli.cli:main'
-        ]
-    },
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
@@ -49,13 +44,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements,
